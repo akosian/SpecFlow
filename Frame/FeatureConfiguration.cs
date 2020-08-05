069cde0e641475c9db55bc9c0ab1f61b20d6c2b5
a.kosyan <a.kosyan@a1qa.com>
2020-08-05 15:08:53 +0300

    move featureConfig

---
+++ Frame/FeatureConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Frame;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace Test
+{
+    [Binding]
+    public class FeatureConfiguration
+    {
+        private IWebDriver Driver;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            Driver = Browser.GetInstance();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Driver.Close();
+        }
+    }
+}
